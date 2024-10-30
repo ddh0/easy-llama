b0025585ce57973d7bcbfb4fa994598f8ab56965
ddh0 <dylanhalladay02@icloud.com>
2024-10-30 13:38:24 -0500

    update values for settings.js buttons

--- easy_llama/assets/settings.js
+++ easy_llama/assets/settings.js
@@ -51,10 +51,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.getElementById('setDefaultsButton').addEventListener('click', function() {
         document.getElementById('max_len_tokens').value = -1;
-        document.getElementById('top_k').value = 40;
-        document.getElementById('top_p').value = 0.95;
-        document.getElementById('min_p').value = 0.05;
-        document.getElementById('temp').value = 0.8;
+        document.getElementById('top_k').value = -1;
+        document.getElementById('top_p').value = 1.0;
+        document.getElementById('min_p').value = 0.1;
+        document.getElementById('temp').value = 1.0;
         document.getElementById('frequency_penalty').value = 0.0;
         document.getElementById('presence_penalty').value = 0.0;
         document.getElementById('repeat_penalty').value = 1.0;
